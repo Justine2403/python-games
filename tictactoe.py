597297c0ba4b0d77c2295be2412c54665b21e2b1
Justine0324 <justine.xiang@ensea.fr>
2025-11-18 16:50:35 +0100

    add games

--- tictactoe.py
+++ tictactoe.py
@@ -4,7 +4,4 @@ import random
 def tictactoe():
     print("Choose a position among this pattern: \n 0 | 1 | 2 \n 3 | 4 | 5 \n 6 | 7 | 8 ")
 
-def __main__():
-    tictactoe()
-
-__main__()
+tictactoe()
