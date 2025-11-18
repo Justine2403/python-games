e5860717e097f28659f02ef65e81c269ddebb80a
Justine0324 <justine.xiang@ensea.fr>
2025-11-18 16:43:20 +0100

    add tic tac toe

---
+++ tictactoe.py
@@ -0,0 +1,10 @@
+import math 
+import random
+
+def tictactoe():
+    print("Choose a position among this pattern: \n 0 | 1 | 2 \n 3 | 4 | 5 \n 6 | 7 | 8 ")
+
+def __main__():
+    tictactoe()
+
+__main__()
